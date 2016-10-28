eacff62290c67ef5fc59b628a363be406ba35d23
Sean McElroy <smcelroy@moderndemocracy.com>
2016-10-28 12:22:37 +0100

    updates to inital application

---
+++ count-manager/cm-web/src/main/java/com/modern/democracy/CountManagerApplication.java
@@ -0,0 +1,54 @@
+package com.modern.democracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version $Id: $
+ */
+@SpringBootApplication
+public class CountManagerApplication {
+
+    // ===========================================
+    // Public Members
+    // ===========================================
+
+    // ===========================================
+    // Private Members
+    // ===========================================
+
+    // ===========================================
+    // Static initialisers
+    // ===========================================
+
+    // ===========================================
+    // Constructors
+    // ===========================================
+
+    public CountManagerApplication() {
+        super();
+    }
+        
+    // ===========================================
+    // Public Methods
+    // ===========================================
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     * @throws Exception the exception
+     */
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(CountManagerApplication.class, args);
+    }
+
+    // ===========================================
+    // Protected Methods
+    // ===========================================
+
+    // ===========================================
+    // Private Methods
+    // ===========================================
+
+}
